b71bf6757bad5bbbe6c75a76cfae6224a48ecd87
Luca Becker <luca.becker@sunbury.xyz>
2020-05-22 12:49:13 +0200

    fixing dependencies & bug
    the plugin would not expose any services, because they weren't added to the array. Also moved the dependencies to devDependencies because Homebridge doesn't want that.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
-import { Characteristic, Service, CharacteristicEventTypes, CharacteristicGetCallback } from 'hap-nodejs';
-import { HomebridgeHCSR501Config } from './types';
-import { MotionSensor } from 'hap-nodejs/dist/lib/gen/HomeKit';
-import { Gpio, BinaryValue } from 'onoff';
+import {Characteristic, Service, CharacteristicEventTypes, CharacteristicGetCallback} from 'hap-nodejs';
+import {HomebridgeHCSR501Config} from './types';
+import {Gpio, BinaryValue} from 'onoff';
+import {AccessoryInformation, MotionSensor} from "hap-nodejs/dist/lib/gen/HomeKit";
+import {API, Logging} from "homebridge";
 
-let homebridgeService;
+let HBService: typeof Service;
 
-export default function (homebridge: any): void {
-    homebridgeService = homebridge.hap.Service;
-
+export default function (homebridge: API): void {
     homebridge.registerAccessory("homebridge-hc-sr501", "HC-SR501", HomebridgeSR501Sensor);
 };
 
@@ -16,27 +15,42 @@ class HomebridgeSR501Sensor {
 
     private readonly gpio: Gpio;
 
-    private motionSensorService: MotionSensor;
+    private readonly informationService: AccessoryInformation;
+
+    private readonly motionSensorService: MotionSensor;
 
     private readonly services: Service[] = [];
 
-    constructor(log: any, private config: HomebridgeHCSR501Config) {
+    constructor(private readonly log: Logging,
+                private readonly config: Partial<HomebridgeHCSR501Config>,
+                private readonly api: API) {
+
+        HBService = api.hap.Service;
+
+        // info service
+        this.informationService = new Service.AccessoryInformation(this.config.name);
+
+        this.informationService
+            .setCharacteristic(Characteristic.Manufacturer, "PIR Manufacturer")
+            .setCharacteristic(Characteristic.Model, config.model || "HC-SR501")
+            .setCharacteristic(Characteristic.SerialNumber, config.serial || "4BD53931-D4A9-4850-8E7D-8A51A942FA29");
+
         this.gpio = new Gpio(this.config.pinId, 'in', 'both');
-        this.motionSensorService = new homebridgeService.MotionSensor(this.config.name, 'sr501_sensor_information');
-        this.motionSensorService.getCharacteristic(Characteristic.MotionDetected)?.
-            on(CharacteristicEventTypes.GET, this.getState.bind(this));
+        this.motionSensorService = new HBService.MotionSensor(this.config.name);
+        this.motionSensorService.getCharacteristic(Characteristic.MotionDetected)?.on(CharacteristicEventTypes.GET, this.getState.bind(this));
 
         this.gpio.watch((err: Error, value: BinaryValue) => {
             if (!err) {
                 this.motionSensorService.getCharacteristic(Characteristic.MotionDetected)?.updateValue(value);
             } else {
-                log.log(err);
+                this.log(err.name, err.message);
             }
         });
+        this.services.push(this.informationService, this.motionSensorService);
     }
 
     private getState(callback: CharacteristicGetCallback) {
-        var val = this.gpio.readSync();
+        const val: BinaryValue = this.gpio.readSync();
         callback(null, val);
     }
 
